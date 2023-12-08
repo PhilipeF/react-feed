0d62311e21845b0eb78f1ad3770bcfbec7421d65
PhilipeF <philipesferreiraa@gmail.com>
2023-12-08 15:26:38 -0300

    Feat: Clear Warns

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,13 +25,11 @@ export function Post({ author, publisheAt, content }) {
         setComments([...comments, newCommentText])
 
         setNewComentText('')
-
     }
 
     function handleNewCommentChange() {
         setNewComentText(event.target.value)
     }
-
 
     const publishedDateRelativeToNow = formatDistanceToNow(publisheAt, {
         locale: ptBR,
@@ -56,9 +54,9 @@ export function Post({ author, publisheAt, content }) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>;
+                        return <p key={line.content}>{line.content}</p>;
                     } else if (line.type === 'link') {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div >
@@ -76,7 +74,7 @@ export function Post({ author, publisheAt, content }) {
             </form>
             <div className={styles.commetList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article >
